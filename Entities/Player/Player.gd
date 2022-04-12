963b8c17f9085593b04a0189527a8d26f54f750e
tiglapiles <tiglapiles@gmail.com>
2022-04-12 16:40:11 +0800

    add level1 component

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -1,6 +1,15 @@
 extends KinematicBody2D
 
 export var move_speed := 200.0
+onready var camera := $Position2D/Camera2D
+onready var level := $"../.."
+
+
+func _ready():
+	camera.set_limit(MARGIN_LEFT, level.get_limit().left)
+	camera.set_limit(MARGIN_TOP, level.get_limit().top)
+	camera.set_limit(MARGIN_RIGHT, level.get_limit().right)
+	camera.set_limit(MARGIN_BOTTOM, level.get_limit().bottom)
 
 
 func _physics_process(delta: float) -> void:
@@ -13,8 +22,6 @@ func _get_direction() -> Vector2:
 		(Input.get_action_strength("right") - Input.get_action_strength("left")) * 2.0,
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	).normalized()
-
-
 
 
 func _on_PickupRadius_area_entered(area: Area2D) -> void:
---
+++ Level/Level.gd
@@ -0,0 +1,32 @@
+extends Node2D
+class_name level
+
+export var move_limit_left := -10000000
+export var move_limit_top := -100000000
+export var move_limit_right := 100000000
+export var move_limit_bottom := 100000000
+
+var BARRIER_ID := 1
+var INVISIBLE_BARRIER_ID := 2
+var _tracker := EntityTracker.new()
+
+onready var _ground := $GroundTile
+onready var _entity_place := $YSort/EntityPlacer
+onready var _player := $YSort/Player
+onready var _flat_entities := $FlatEntities
+onready var _gui := $"../../CanvasLayer/GUI"
+
+func _ready():
+	_entity_place.setup(_gui, _tracker, _ground, _flat_entities, _player)
+	var barriers: Array = _ground.get_used_cells_by_id(BARRIER_ID)
+	for cell in barriers:
+		_ground.set_cellv(cell, INVISIBLE_BARRIER_ID)
+
+
+func get_limit() -> Dictionary:
+	return {
+		left = move_limit_left,
+		top = move_limit_top,
+		right = move_limit_right,
+		bottom = move_limit_bottom,
+	}
