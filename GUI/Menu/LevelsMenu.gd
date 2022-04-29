33213a5838d6b594b6c8e11a35c9a9d2a7a2267d
tiglapiles <tiglapiles@gmail.com>
2022-04-29 08:58:20 +0800

    decrete level

--- GUI/Menu/LevelsMenu.gd
+++ GUI/Menu/LevelsMenu.gd
@@ -4,10 +4,7 @@ onready var button1 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContaine
 onready var button2 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Button2
 onready var button3 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Button3
 onready var button4 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Button4
-onready var button5 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Button5
-onready var button6 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Button6
-onready var button7 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Button7
-onready var button8 := $MarginContainer/VBoxContainer/VBoxContainer/HBoxContainer/Button8
+
 
 
 func _ready() -> void:
@@ -15,10 +12,6 @@ func _ready() -> void:
 	button2.connect("pressed", self, "_go_to_level", ["level2"])
 	button3.connect("pressed", self, "_go_to_level", ["level3"])
 	button4.connect("pressed", self, "_go_to_level", ["level4"])
-	button5.connect("pressed", self, "_go_to_level", ["level5"])
-	button6.connect("pressed", self, "_go_to_level", ["level6"])
-	button7.connect("pressed", self, "_go_to_level", ["level7"])
-	button8.connect("pressed", self, "_go_to_level", ["level8"])
 
 
 func _go_to_level(value: String) -> void:
